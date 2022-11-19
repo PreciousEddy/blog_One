d2d17f03fbd5a2cf55928a98caac1a67e2d1bd60
Precious Edmund <edmundprecious23@gmail.com>
2022-11-19 15:21:48 +0100

    Update

--- Blog/blogApp/urls.py
+++ Blog/blogApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
-    path('about/', views.about, name= 'blog-about')
+    path('', views.home, name ='blogApp-home'),
+    path('about/', views.about, name='blogApp-about')
 ]
